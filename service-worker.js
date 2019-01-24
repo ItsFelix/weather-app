9928330bd55a31b22ca33f3db27f3213ae6da974
Felix <f.samplexyt@gmail.com>
2019-01-24 16:02:21 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.5efca66aee47997109270384e09cd8ec.js"
+  "/weather-app/precache-manifest.b321f788a7f32aa6da2dfee0d8d31e19.js"
 );
 
 workbox.clientsClaim();
